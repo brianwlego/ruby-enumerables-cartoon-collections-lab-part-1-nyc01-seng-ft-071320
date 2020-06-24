f5666073aaaa38ca377f37a1ee65ce70073c709a
Brian Lego <legobw@gmail.com>
2020-06-24 20:04:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index { |name, index+1|
-    p "/#{index}.*#{name}/" 
+  array.each_with_index { |name, index|
+    p "/#{index+1}.*#{name}/" 
   }
   
 end
